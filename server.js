27aa7c565819cee1cbf2c53a50ae8920ab1606d6
presstodash <130226667+presstodash@users.noreply.github.com>
2024-10-26 21:46:38 +0200

    Update server.js
    Bugfix da radi za produkciju umjesto localhost

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.AUTH0_SECRET,
-    baseURL: 'http://localhost:3000',
+    baseURL: BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
 };
