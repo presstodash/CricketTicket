00da2fcb8338f975843ba1681c206ba382f97b05
presstodash <130226667+presstodash@users.noreply.github.com>
2024-10-27 13:22:48 +0100

    nastavak bugfixing

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'very-secret-randomly-generated-strong-key',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === 'production' }
+    cookie: { secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+    }
 }));
 
 const PORT = process.env.PORT || 3000;
